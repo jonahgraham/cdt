271b77e96560209d3069e45c86a17365dfe1577c
Jonah Graham <jonah@kichwacoders.com>
2024-06-23 17:02:28 -0400

    WIP

--- releng/scripts/do_format_code.sh
+++ releng/scripts/do_format_code.sh
@@ -44,7 +44,9 @@ fi
     $PWD -data check_code_cleanliness_workspace &
 PID=$!
 
+set +e
 while true; do
+  sleep 1s
   echo
   echo "Contents of log"
   cat check_code_cleanliness_workspace/.metadata/.log
